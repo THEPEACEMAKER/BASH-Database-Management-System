c73932610c4a03efcd7fe2fd791269e2b85e1fd5
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2022-12-16 23:31:54 +0200

    useDB

--- useDB.sh
+++ useDB.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "useDB"
+echo "Enter database name: "
+read currDB;
+
+# check if database exists
+if [[ -d Databases/$currDB ]]
+then
+    echo "$currDB is selected.";
+    export  currDB=Databases/$currDB;
+    . ./tablesMenu.sh;
+else
+    echo "Database does not exist.";
+fi
