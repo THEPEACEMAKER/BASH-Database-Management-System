72cc9d67f6330ecb9c4199b87a1bd01e0c5e3940
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2022-12-17 00:17:58 +0200

    delete database

--- deleteDB.sh
+++ deleteDB.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "deleteDB"
+read -p "Enter Database name: " databaseName;
+
+if [ -d  "Databases/$databaseName" ]; then
+    rm -r Databases/$databaseName
+    echo "$databaseName database  deleted Successfully"
+  else
+    echo "No such Database"
+fi
