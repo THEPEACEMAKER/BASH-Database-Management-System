f43b67695db7b3f463d6c3fa4ea7f20c6facbf25
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2022-12-16 23:37:25 +0200

    add tables scripts and menu

--- tablesMenu.sh
+++ tablesMenu.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo "using $currDB"
+tables_menu(){
+    options=("Show Tables" "Create New Table" "Delete Table" "Update Table" "Display Table" "Return To Main Menu");
+
+    select option in "${options[@]}"
+    do
+        case $option in
+            "Show Tables") . ./showTables.sh;;
+            "Create New Table") . ./createTable.sh;;
+            "Delete Table") . ./deleteTable.sh;;
+            "Update Table") . ./updateTable.sh;;
+            "Display Table") . ./displayTable.sh;;
+            "Return To Main Menu") break;;
+            *) echo "Invalid option $REPLY";;
+        esac
+    done
+}
+
+tables_menu
