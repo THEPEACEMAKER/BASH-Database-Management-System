ffd41115cb6f19e3a05fbd25b60ac30a37ff3df1
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2022-12-17 20:42:58 +0200

    delete table

--- deleteTable.sh
+++ deleteTable.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "deleteTable"
+read -p "Enter Table Name: " TableName;
+
+if [ -f  "$currDB/$TableName.idb" ] && [ -f  "$currDB/.$TableName.frm" ]; then
+    rm -r $currDB/$TableName.idb
+    rm -r $currDB/.$TableName.frm
+    echo "$TableName Table  deleted Successfully"
+  else
+    echo "No such Table"
+fi
