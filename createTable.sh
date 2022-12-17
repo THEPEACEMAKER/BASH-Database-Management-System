d9e7b06a4b5dd74bb66eee0f1eaafad5107c5ac2
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2022-12-17 04:46:38 +0200

    createTableFiles

--- createTable.sh
+++ createTable.sh
@@ -1,3 +1,44 @@
 #!/bin/bash
 
-echo "createTable"
+read -p "Enter table name: " tableName;
+
+# Validate the entered name
+while ! [[ $tableName =~ ^([a-zA-Z])[a-zA-Z0-9\w_-]*([a-zA-Z0-9])$ ]]; do
+    echo "$tableName is not a valid name";
+    echo "table names should not have any special characters, spaces, doesn't start with a number or end with a '-' or '_'";
+    echo ""
+    read -p "Enter table name: " tableName;
+done
+
+function createTableFiles(){
+    currDB=$1;
+    tableName=$2;
+
+    success=0
+
+    # create tableName.idb
+    if touch "$currDB/$tableName.idb" 2>> log.out; then
+        echo "Data table created sucessfully." >> log.out;
+        # create tableName.frm
+        if touch "$currDB/$tableName.frm" 2>> log.out; then
+            echo "Format file created sucessfully." >> log.out;
+        else
+            echo "Falied to create the format file. Check log.out for more details.";
+            success=1
+        fi
+    else
+        echo "Falied to create data table. Check log.out for more details.";
+        success=1
+    fi
+
+    echo $success;
+}
+
+# main
+if test -f $currDB/$tableName 2> log.out; then
+	echo "Table already exists. Check log.out for more details.";
+else
+    if [ $(createTableFiles "$currDB" "$tableName")  == 0 ]; then
+    	echo "Table files created successfully" >> log.out;
+    fi
+fi
--- useDB.sh
+++ useDB.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# [TO-DO: if no database is entered the check -d Databases/$currDB still pass cause of the dir Databases ]
+
 read -p "Enter Database name: " currDB;
 
 # check if database exists
-if [[ -d {Databases/$currDB} ]]
+if [[ -d Databases/$currDB ]]
 then
     echo "$currDB is selected.";
     echo ""
