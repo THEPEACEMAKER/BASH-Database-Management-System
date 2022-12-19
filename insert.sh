2f1073765b2ddc36ed7b0576e7f34d3526ba3b07
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2022-12-19 02:56:59 +0200

    insert into table

---
+++ insert.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# for debugging
+# currDB="Databases/iti"
+# tableName="emp";
+
+tableData="$currDB/$tableName.idb"
+tableFormat="$currDB/.$tableName.frm"
+
+newRecord="";
+
+# read columns info from tableName.metadata into array
+read -d '\n' -r -a lines < "$tableFormat"
+
+for i in "${!lines[@]}" # indices of the array 0,1,2
+do
+    # read column attributs
+    IFS=':' read -r -a column <<< "${lines[i]}";
+    name=${column[0]};
+    dataType=${column[1]};
+    PK=${column[2]};
+
+    #read new column value from user
+    read -p "Enter $name: " value;
+    
+    # validate the new value type
+    if [[ $dataType == "number" ]]; then
+        while ! [[ $value =~ ^[0-9]+$ ]]; do
+            echo "$name must be a number.";
+            echo ""
+            read -p "Enter $name: " value;
+        done
+    elif [[ $dataType == "string" ]]; then
+        while ! [[ $value =~ ^[a-zA-Z]+$ ]]; do
+            echo "$name must be a string.";
+            echo ""
+            read -p "Enter $name: " value;
+        done
+    fi
+
+    # validate if PK
+    if [[ $PK == "yes" ]]; then
+        # get all column data from Data/tableName
+        read -d '' -r -a dataLines < "$tableData"  # all table
+        
+        #loop over column data to check pk if unique
+        for j in "${!dataLines[@]}";
+        do
+            IFS=':' read -r -a record <<< "${dataLines[$j]}"; # record(row)
+            while [[ ${record[i]} == $value ]]; do
+                echo "$name is a primary key and must be unique.";
+                echo ""
+                read -p "Enter $name: " value;
+            done
+        done
+    fi
+
+    if [[ $i == 0 ]]; then
+        newRecord=$value;
+    else
+        newRecord+=":$value";
+    fi
+done
+
+if ! [[ $newRecord == "" ]]; then
+    if echo $newRecord >> "$tableData"; then
+        echo "Record stored succesfully.";
+    else
+        echo "ERROR: Failed to store record.";
+    fi
+else
+    echo "ERROR: Record is empty.";
+fi
+
+echo ""
+. ./tableMenu.sh
