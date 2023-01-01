a734220e64c86efc0a4b483ba4f3fc0a883695eb
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2023-01-01 10:39:06 +0200

    validate deleteDB input

--- deleteDB.sh
+++ deleteDB.sh
@@ -2,6 +2,13 @@
 
 read -rp "Enter Database name: " databaseName;
 
+while ! [[ $currDB =~ ^([a-zA-Z])[a-zA-Z0-9\w_-]*([a-zA-Z0-9])$ ]]; do
+    echo "$currDB is not a valid name";
+    echo "database names should not have any special characters, spaces, doesn't start with a number or end with a '-' or '_'";
+    echo ""
+    read -rp "Enter database name: " currDB;
+done
+
 if [ -d  "Databases/$databaseName" ]; then
     rm -r Databases/$databaseName
     echo "$databaseName database  deleted Successfully"
