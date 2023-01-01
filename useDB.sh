69d1eb38abe01e8227a676d3f25ca306fae12cec
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2023-01-01 10:13:57 +0200

    validate useDB input

--- useDB.sh
+++ useDB.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# [TO-DO: if no database is entered the check -d Databases/$currDB still pass cause of the dir Databases ]
-
 read -rp "Enter Database name: " currDB;
+
+while ! [[ $currDB =~ ^([a-zA-Z])[a-zA-Z0-9\w_-]*([a-zA-Z0-9])$ ]]; do
+    echo "$currDB is not a valid name";
+    echo "database names should not have any special characters, spaces, doesn't start with a number or end with a '-' or '_'";
+    echo ""
+    read -rp "Enter database name: " currDB;
+done
+
 
 # check if database exists
 if [[ -d Databases/$currDB ]]
